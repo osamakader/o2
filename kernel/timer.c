560a2a46a8995aa0bd34d37a2bd17dd03ac1f0bf
Osama Abdelkader <osama.abdelkader@gmail.com>
2026-05-11 23:47:53 +0200

    one sec tick timer
    Signed-off-by: Osama Abdelkader <osama.abdelkader@gmail.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -5,7 +5,7 @@ void init_timer() {
 
     uint64_t freq;
     asm volatile("mrs %0, CNTFRQ_EL0" : "=r" (freq));
-    asm volatile("msr CNTV_TVAL_EL0, %0" : : "r" (10000));
+    asm volatile("msr CNTV_TVAL_EL0, %0" : : "r" (freq));
     uint64_t ctl = 1;
     asm volatile("msr CNTV_CTL_EL0, %0" : : "r" (ctl));
     asm volatile("isb");
